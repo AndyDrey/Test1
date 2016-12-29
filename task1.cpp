ad1137b7ba260357381b37a1b922e14d87be9a61
AndyDrey <andy.yarmol@gmail.com>
2016-12-29 20:24:48 +0200

    task1

--- task1.cpp
+++ task1.cpp
@@ -1,65 +1,48 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class HTMLElemen{
 public:
-    unsigned Width() const;
-    unsigned Height() const;
-    bool Hidden() const;
-    virtual HTMLElemen* duplicate() = 0;
-    virtual ~HTMLElemen(){}
+	unsigned Width() const;
+	unsigned Height() const;
+	bool Hidden() const;
+	virtual string Render() = 0;
 };
 
 class HTMLButtonElement : public HTMLElemen{
 public:
-    const string& Title() const;
-    string Render() override{
-        return "<button>title-of-the-button</button>\n";
-    }
-    HTMLElemen* duplicate()
-    {
-        return new HTMLButtonElement(*this);
-    }
+	const string& Title() const;
+	string Render() override{
+		return "<button>title-of-the-button</button>\n";
+	}
 };
 
 class HTMLImageElement : public HTMLElemen{
 public:
-    const string& Url() const;
-    string Render() override{
-        return "<img src=\"../../smiley.gif\" height=\"42\" width=\"42\">\n";
-    }
-    HTMLElemen* duplicate()
-    {
-        return new HTMLImageElement(*this);
-    }
+	const string& Url() const;
+	string Render() override{
+		return "<img src=\"../../smiley.gif\" height=\"42\" width=\"42\">\n";
+	}
 };
 
 class HTMLTextAreaDocument : public HTMLElemen{
 public:
-    const string& Content() const;
-    string Render() override{
-        return "<textarea>\ncontent-of-the-text-area\n</textarea>\n";
-    }
-    HTMLElemen* duplicate()
-    {
-        return new HTMLTextAreaDocument(*this);
-    }
+	const string& Content() const;
+	string Render() override{
+		return "<textarea>\ncontent-of-the-text-area\n</textarea>\n";
+	}
 };
 
 
 int main(int argc, char *argv[])
 {
-    HTMLElemen *p;
-    HTMLButtonElement obj1;
-    HTMLImageElement obj2;
-    HTMLTextAreaDocument obj3;
-    p = &obj1;
-    cout << p->Render();
-    p =&obj2;
-    cout << p->Render();
-    p =&obj3;
-    cout << p->Render();
-    delete p;
-    return 0;
+	HTMLButtonElement a1;
+	HTMLImageElement a2;
+	HTMLTextAreaDocument a3;
+	cout << a1.Render();
+	cout << a2.Render();
+	cout << a3.Render();
+	return 0;
 }
